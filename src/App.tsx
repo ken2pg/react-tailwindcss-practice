7d01be4da3b29f2c3afb6a78ae2f86a628e4948f
kento <jst34zais9102z@gmail.com>
2021-11-01 15:09:31 +0900

    feat：usecontextの導入、redux関連機能を別ファイルに移動

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, createContext, useContext } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import SignIn from "./components/signup/signIn";
@@ -6,61 +6,34 @@ import SignUp2 from "./components/signup/signUp2";
 
 import { Redirect, Route, Switch, Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import {
+  ACTIONTYPE,
+  initialState,
+  reducer,
+  STORE,
+} from "./duck/singup/reducer";
 
 export const history = createBrowserHistory();
 
-type STORE = {
-  Email: string;
-  password: string;
-  userId: string;
-  userName: string;
-};
-
-type ACTIONTYPE =
-  | {
-      type: "signup";
-      payload: {
-        Email: string;
-        password: string;
-        userName: string;
-      };
-    }
-  | { type: "signin" };
-
-const initialState: STORE = {
-  Email: "",
-  password: "",
-  userId: "",
-  userName: "",
-};
-
-const reducer = (state: STORE, action: ACTIONTYPE) => {
-  switch (action.type) {
-    case "signup":
-      return {
-        Email: action.payload.Email,
-        password: action.payload.password,
-        userId: state.userId,
-        userName: action.payload.userName,
-      };
-    default:
-      return state;
-  }
-};
+const AppContext = createContext(
+  {} as { state: STORE; dispatch: React.Dispatch<ACTIONTYPE> }
+);
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="mt-52 w-full flex justify-center">
-      {/* <SignIn /> */}
-      <Router history={history}>
-        <Route path="/signin">
-          <SignIn />
-        </Route>
-        <Route path="/signup">
-          <SignUp2 />
-        </Route>
-      </Router>
+      <AppContext.Provider value={{ state, dispatch }}>
+        <Router history={history}>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/signup">
+            <SignUp2 />
+          </Route>
+        </Router>
+      </AppContext.Provider>
     </div>
   );
 }
